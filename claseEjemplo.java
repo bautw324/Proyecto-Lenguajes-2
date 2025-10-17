2020885fead4dbc7f29c1669cb12fcfbfd483982
juangiron06 <juanmanuelgiron06@gmail.com>
2025-10-17 17:12:13 -0300

    Pitito

--- claseEjemplo.java
+++ claseEjemplo.java
@@ -1,3 +1,12 @@
-class claseEjemplo{
+class claseEjemplo {
+    public String pitito = "moooooy grande";
+    public String puto = "micaias";
 
+    public claseEjemplo() {
+        pitito = this.pitito;
+    }
+
+    public void comerla(String nombre) {
+        System.out.println(nombre + "se come un pitito " + pitito);
+    }
 }
